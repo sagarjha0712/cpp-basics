9b43ddb75dc281f6d1d27d7cf4eeefa24a9134a5
Sagar Kumar Jha <sagarjha0712@gmail.com>
2026-08-22 23:22:15 +0530

    0047-PERMUTATIONS-II Stats: Time: 0 ms (100%), Space: 11.3 MB (98.2%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,27 +1,13 @@
 class Solution {
-
-private:
-    void recurPermute(int index,vector<int>& nums,vector<vector<int>> &ans ) {
-        if (index==nums.size()) {
-            ans.push_back(nums);
-            return;
-        }
-
-        unordered_set<int>seen;
-
-        for (int i=index;i<nums.size();i++) {
-            if (seen.count(nums[i])) continue;
-            seen.insert(nums[i]);
-            swap(nums[index],nums[i]);
-            recurPermute(index+1,nums,ans);
-            swap(nums[index],nums[i]);
-        }
-    }
-
+  
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
-        recurPermute(0,nums,ans);
+        sort(nums.begin(), nums.end());
+        
+        do {
+            ans.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
         return ans;
     }
 };
