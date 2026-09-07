9adfecbd6993118d5553f1e7fa5d1a42e759ad29
Sagar Kumar Jha <sagarjha0712@gmail.com>
2026-09-07 22:51:46 +0530

    0229-MAJORITY-ELEMENT-II Stats: Time: 0 ms (100%), Space: 23.8 MB (50.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,45 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-        vector<int>major;
+        int count1 =0,el1;
+        int count2=0,el2;
+        vector<int> major;
 
         for (int i=0;i<nums.size();i++) {
-            mpp[nums[i]]++;
+            if (count1 ==0 && nums[i]!=el2) {
+                count1+=1;
+                el1=nums[i];
+            }
+            else if (count2 ==0 && nums[i]!=el1) {
+                count2+=1;
+                el2=nums[i];
+            }
+            else if (el1==nums[i]) {
+                count1++;
+
+            }
+            else if (el2==nums[i]) {
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }
         }
 
-        for (auto it :mpp) {
-            if (it.second>nums.size()/3) {
-                major.push_back(it.first);
+        int c1=0,c2=0;
+
+        for (int i=0;i<nums.size();i++) {
+            if (el1==nums[i]) {
+                c1++;
+            }else if (el2==nums[i]) {
+                c2++;
             }
+        }
+        if (c1>nums.size()/3) {
+            major.push_back(el1);
+        }
+        if (c2>nums.size()/3) {
+            major.push_back(el2);
         }
 
         return major;
