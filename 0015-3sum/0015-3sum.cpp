8f4b21779143738a31c4ffa82c440d1cb6929350
Sagar Kumar Jha <sagarjha0712@gmail.com>
2026-09-10 02:29:24 +0530

    0015-3SUM Stats: Time: 46 ms (66.56%), Space: 29 MB (92.31%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,33 +5,33 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         
-        for (int i = 0; i < n - 2; ++i) {
+        for (int i = 0; i < n; ++i) {
             if (nums[i] > 0) break;
             
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             
-            int left = i + 1;
-            int right = n - 1;
+            int j = i + 1;
+            int k = n - 1;
             
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
                 
                 if (sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
+                    result.push_back({nums[i], nums[j], nums[k]});
                     
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
                     }
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
                     }
+                    j++;
+                    k--;
                     
-                    left++;
-                    right--;
                 } else if (sum < 0) {
-                    left++;
+                    j++;
                 } else {
-                    right--;
+                    k--;
                 }
             }
         }
